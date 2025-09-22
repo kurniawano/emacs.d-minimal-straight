1a6838c1897c7455be57dd5838ffa3b51ac87630
Oka Kurniawan <oka@okakurniawan.net>
2025-09-22 16:58:20 +0800

    fix markdown function.

--- post-init.el
+++ post-init.el
@@ -52,12 +52,11 @@
 
 (straight-use-package 'impatient-mode)
 
- <!-- language: lang-lisp -->
 
-  (defun markdown-html (buffer)
-    (princ (with-current-buffer buffer
-      (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://ndossougbe.github.io/strapdown/dist/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
-           (current-buffer)))
+(defun markdown-html (buffer)
+  (princ (with-current-buffer buffer
+           (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://ndossougbe.github.io/strapdown/dist/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
+         (current-buffer)))
 
 ;; org configuration
 (require 'org)
